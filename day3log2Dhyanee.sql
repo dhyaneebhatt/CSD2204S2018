596fb8a7270c0b931377e89bf379933bbc5f60fd
dhyaneebhatt <39058646+dhyaneebhatt@users.noreply.github.com>
2018-05-09 11:35:40 -0400

    Add files via upload

---
+++ day3log2Dhyanee.sql
@@ -0,0 +1,9 @@
+MariaDB [CSD2204S18]> SHOW TABLES;
++----------------------+
+| Tables_in_csd2204s18 |
++----------------------+
+| customer             |
++----------------------+
+1 row in set (0.00 sec)
+
+MariaDB [CSD2204S18]> EXIT;
